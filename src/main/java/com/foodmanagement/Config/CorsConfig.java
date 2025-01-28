068716ee99ae491dcecd3e8d4dcad2100a77a491
Dhaneera <wickramasuriyashehan@gmail.com>
2025-01-28 23:09:23 +0530

    datab

--- src/main/java/com/foodmanagement/Config/CorsConfig.java
+++ src/main/java/com/foodmanagement/Config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://746z48sk-3000.asse.devtunnels.ms/") // Replace with your frontend URL
+                .allowedOriginPatterns("http://*","https://*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true) // Allow cookies, if any
                 .maxAge(3600);
