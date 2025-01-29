3d382c18a5ecb77ef7f44a417ea04753e7ee8e3a
Dashan Nadeema <dashan.nadeema@tecciance.com>
2025-01-29 17:24:10 +0530

    deployment issue check

--- src/main/java/com/foodmanagement/Controller/AuthController.java
+++ src/main/java/com/foodmanagement/Controller/AuthController.java
@@ -34,7 +34,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@CrossOrigin
+@CrossOrigin("*")
 @RequestMapping("/api/auth")
 public class AuthController {
     private final AuthenticationManager authenticationManager;
